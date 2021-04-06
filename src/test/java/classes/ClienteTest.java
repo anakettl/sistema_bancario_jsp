958e0db48e53e62b57c900b2fd3b448734e2d877
anakettl <anakettl@gmail.com>
2021-04-06 01:12:49 -0300

    Add pmd rules and ClienteTest

---
+++ src/test/java/classes/ClienteTest.java
@@ -0,0 +1,37 @@
+package classes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClienteTest {
+    private Cliente cliente;
+
+    @BeforeEach
+    public void setUp(){
+        cliente = new Cliente();
+    }
+
+    @Test
+    void registerClienteWithInvalidCpf(){
+        cliente.setCpf("1243");
+
+        assertEquals(null, cliente.getCpf());
+    }
+
+    @Test
+    void registerClienteWithInvalidEmail(){
+        cliente.setEmail("");
+
+        assertEquals(null, cliente.getEmail());
+    }
+
+    @Test
+    void registerClienteWithInvalidTelefone(){
+        cliente.setTelefone("");
+
+        assertEquals(null, cliente.getTelefone());
+    }
+
+}
